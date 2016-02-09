2118a479b2a516c0f0a0e313731c2842c891ccee
cmgrove <cmgrove@deep-thought.info-science.uiowa.edu>
2016-02-09 22:57:25 +0000

    EdgeLookup tag for automatic edge population.

---
+++ src/main/java/edu/uiowa/slis/graphtaglib/EdgeLookup.java
@@ -0,0 +1,26 @@
+package edu.uiowa.slis.graphtaglib;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class EdgeLookup extends TagSupport {
+	private static final long serialVersionUID = 1L;
+
+	String edgeLocator = null;
+	Class cls = null;
+	// It would be good to check if the Class implements EdgeGenerator...
+	EdgeGenerator edgeGen = null;
+			
+	public int doStartTag() throws JspException {
+		Graph theGraph = (Graph)findAncestorWithClass(this, Graph.class);
+		edgeGen.populateEdges(theGraph);
+		return 1;
+	}
+	
+	public void setLocator(String edgeLocator) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		this.edgeLocator = edgeLocator;
+		this.cls = Class.forName(edgeLocator);
+		this.edgeGen = (EdgeGenerator) cls.newInstance();
+	}
+	
+}
