f30f0c8771d08451b58db0b552e3eec443eab129
cmgrove <cmgrove@deep-thought.info-science.uiowa.edu>
2016-02-16 23:42:09 +0000

    Changed EdgePopulator to abstract class, updated EdgeLookup accordingly. Also updated EdgeLookup to take data source attribute.

--- src/main/java/edu/uiowa/slis/graphtaglib/EdgeLookup.java
+++ src/main/java/edu/uiowa/slis/graphtaglib/EdgeLookup.java
@@ -1,5 +1,8 @@
 package edu.uiowa.slis.graphtaglib;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
@@ -7,20 +10,51 @@ public class EdgeLookup extends TagSupport {
 	private static final long serialVersionUID = 1L;
 
 	String edgeLocator = null;
+	String dataSource = null;
 	Class cls = null;
 	// It would be good to check if the Class implements EdgeGenerator...
 	EdgePopulator edgeGen = null;
+
 			
 	public int doStartTag() throws JspException {
+		try {
+			this.cls = Class.forName(this.edgeLocator);
+			Constructor<?> con = this.cls.getConstructor(String.class);
+			this.edgeGen = (EdgePopulator) con.newInstance(this.dataSource);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		Graph theGraph = (Graph)findAncestorWithClass(this, Graph.class);
 		edgeGen.populateEdges(theGraph);
 		return 1;
 	}
 	
-	public void setLocator(String edgeLocator) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+	public void setLocator(String edgeLocator) {
 		this.edgeLocator = edgeLocator;
-		this.cls = Class.forName(edgeLocator);
-		this.edgeGen = (EdgePopulator) cls.newInstance();
+	}
+	
+	public void setSource(String source) {
+		this.dataSource = source;
 	}
 	
 }
--- src/main/java/edu/uiowa/slis/graphtaglib/EdgePopulator.java
+++ src/main/java/edu/uiowa/slis/graphtaglib/EdgePopulator.java
@@ -1,5 +1,21 @@
 package edu.uiowa.slis.graphtaglib;
 
-public interface EdgePopulator {
-	public void populateEdges(Graph theGraph);
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public abstract class EdgePopulator {
+	
+	DataSource theDataSource = null;
+	
+	public EdgePopulator() {
+		
+	};
+	public EdgePopulator(String source) throws NamingException {
+	     theDataSource = (DataSource) new InitialContext().lookup(source);
+	};
+	public EdgePopulator(DataSource source) {
+		theDataSource = source;
+	}
+	public abstract void populateEdges(Graph theGraph);
 }
